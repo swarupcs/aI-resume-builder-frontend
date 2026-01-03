48f163913113de4476c64a0e477305bf03520ea9
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-01-03 16:28:28 +0530

    feat(app): integrate React Query and add toast notifications - Set up QueryClient and wrapped AppRoutes with QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
-import './App.css'
-import AppRoutes from './AppRoutes.jsx'
-
+import './App.css';
+import AppRoutes from './AppRoutes';
+import { Toaster } from 'sonner';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 function App() {
-
+  const queryClient = new QueryClient();
 
   return (
     <>
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        <Toaster position='top-center' richColors />
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
